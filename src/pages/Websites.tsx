dea95d43a2fa00436b30946e5435fe244e9ad899
madison <madison.sss.ma@gmail.com>
2023-09-19 18:48:00 +0800

    feat: add support me

--- src/pages/Websites.tsx
+++ src/pages/Websites.tsx
@@ -12,6 +12,7 @@ import {Divider} from "../models/components";
 import {defineMessages, useIntl} from "react-intl";
 import LocaleContext from "../contexts/LocaleContext";
 import {LocalesKey} from "../i18n/languages";
+import {Card} from "@mui/material";
 
 const images = {
     linkedin: {
@@ -46,7 +47,8 @@ const messages = defineMessages({
     },
     help: {
         id: 'website.help',
-    }
+    },
+
 });
 const ImageCard = ({src, description}: any) => (
     <div style={{
@@ -79,16 +81,32 @@ const ImageCard = ({src, description}: any) => (
         </CardContent>
     </div>
 )
-
-const Websites = () => {
+const Title = ({content}: any) => (
+    <h4>{content}</h4>
+)
+const DescriptionImage = ({src}: any) => (
+    <img src={src} style={{
+        border: '1px solid var(--color-light-gray)',
+        borderRadius: 8,
+        height: 350,
+        marginRight: 20,
+    }}/>
+)
+const Support = () => {
     const intl = useIntl();
     const {locale} = useContext(LocaleContext);
     return (
         <>
             <CardContent>
-                <section>
-                    <h4>{intl.formatMessage(messages.webs)}：</h4>
-                </section>
+                <Typography variant="h3" gutterBottom>
+                    {intl.formatMessage({id:'website.text.1'})}
+                </Typography>
+                <Typography variant="subtitle2" gutterBottom>
+                    {intl.formatMessage({id:'website.text.2'})}（<a
+                    href="https://chrome.google.com/webstore/detail/it-mercenaries-and-hunter/eilakanollhbgdoppbffeikcbkhmeloc?hl=zh-CN&authuser=0"
+                    target="_blank">{intl.formatMessage(messages.subTitle)}</a>） {intl.formatMessage({id:'website.text.3'})}.
+
+                </Typography>
                 <Swiper
                     // @ts-ignore
                     navigation
@@ -98,42 +116,130 @@ const Websites = () => {
                 >
                     <SwiperSlide>
                         <ImageCard
+                            src={'/imgs/help-5.png'}
+                            description={<a href="https://www.freelancer.com/"
+                                            target="_blank">{intl.formatMessage({id:'website.text.4'})}</a>}
+                        />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <ImageCard
                             src={images.linkedin[locale] ?? "/imgs/linkedin.png"}
                             description={
-                                <a href="https://www.linkedin.com/" target="_blank">LinkedIn</a>
-                            }
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <ImageCard
-                            src={images.freelancer[locale] ?? "/imgs/freelancer.png"}
-                            description={<a href="https://www.freelancer.com/"
-                                            target="_blank">freelancer</a>}
-                        />
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <ImageCard
-                            src={images.upwork[locale] ?? "/imgs/upwork.png"}
-                            description={
-                                <a href="https://www.upwork.com/" target="_blank">upwork</a>
+                                <div>{intl.formatMessage({id:'website.text.5'})}</div>
                             }
                         />
                     </SwiperSlide>
                 </Swiper>
-                <Divider/>
-                <div style={{
-                    textAlign: "left",
-                    marginTop: 20,
+
+                <Card style={{
+                    marginTop: 20
                 }}>
-                    <h3>{intl.formatMessage(messages.title)}(
-                        <a href="https://chrome.google.com/webstore/detail/it-mercenaries-and-hunter/eilakanollhbgdoppbffeikcbkhmeloc?hl=zh-CN&authuser=0"
-                           target="_blank">{intl.formatMessage(messages.subTitle)}</a>
-                        )</h3>
-                    <h4>{intl.formatMessage(messages.description)}</h4>
-                </div>
-                <section>
-                    <div>{intl.formatMessage(messages.info)}</div>
-                </section>
+                    <CardContent>
+                        <h3 style={{textAlign: 'center'}}>{intl.formatMessage({id:'website.text.6'})}</h3>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.7'})}/>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.8'})}/>
+                            {intl.formatMessage({id:'website.text.9'})}
+                            <div>
+                                <DescriptionImage src={'/imgs/help-2.png'}/>
+                            </div>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.10'})}/>
+                            {intl.formatMessage({id:'website.text.10.1'})}
+                            <div>
+                                <DescriptionImage src={'/imgs/help-3.png'}/>
+                            </div>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.11'})}/>
+                            {intl.formatMessage({id:'website.text.12'})}
+                            <div>
+                                <DescriptionImage src={'/imgs/help-4.png'}/>
+                                <DescriptionImage src={'/imgs/help-5.png'} style={{
+                                    width: 350
+                                }}/>
+                            </div>
+                        </section>
+                    </CardContent>
+                </Card>
+                <Card style={{
+                    marginTop: 20
+                }}>
+                    <CardContent>
+                        <h3 style={{textAlign: 'center'}}>{intl.formatMessage({id:'website.text.13'})}</h3>
+
+                        <div style={{
+                            textAlign: "left",
+                            marginTop: 20,
+                        }}>
+                            <h5>{intl.formatMessage(messages.title)}</h5>
+                            <h5>{intl.formatMessage(messages.description)}</h5>
+                        </div>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.14'})}/>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.15'})}/>
+                            {intl.formatMessage({id:'website.text.16'})}
+                            <div>
+                                <DescriptionImage src={'/imgs/help-2.png'}/>
+                            </div>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.17'})}/>
+                            {intl.formatMessage({id:'website.text.18'})}
+                            <div>
+                                <DescriptionImage src={'/imgs/help-1.png'}/>
+                            </div>
+                        </section>
+                        <section>
+                            <Title content={intl.formatMessage({id:'website.text.19'})}/>
+                            {intl.formatMessage({id:'website.text.20'})}
+                        </section>
+                        <Swiper
+                            // @ts-ignore
+                            navigation
+                            modules={[Navigation]}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}
+                        >
+                            <SwiperSlide>
+                                <ImageCard
+                                    src={images.linkedin[locale] ?? "/imgs/linkedin.png"}
+                                    description={
+                                        <a href="https://www.linkedin.com/" target="_blank">LinkedIn</a>
+                                    }
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <ImageCard
+                                    src={images.freelancer[locale] ?? "/imgs/freelancer.png"}
+                                    description={<a href="https://www.freelancer.com/"
+                                                    target="_blank">freelancer</a>}
+                                />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <ImageCard
+                                    src={images.upwork[locale] ?? "/imgs/upwork.png"}
+                                    description={
+                                        <a href="https://www.upwork.com/" target="_blank">upwork</a>
+                                    }
+                                />
+                            </SwiperSlide>
+                        </Swiper>
+                        <Divider/>
+                        <section>
+                            <h4>{intl.formatMessage(messages.webs)}: LinkedIn/Upwork/Freelancer</h4>
+                        </section>
+                        <section>
+                            <div>{intl.formatMessage(messages.info)}</div>
+                        </section>
+                    </CardContent>
+                </Card>
+
                 <section>
                     <div style={{
                         padding: '12px 0',
@@ -146,4 +252,4 @@ const Websites = () => {
     )
 }
 
-export default Websites
+export default Support
