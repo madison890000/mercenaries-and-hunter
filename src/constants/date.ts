56c3390e4a8390b10247a2db772ba703046872c6
madison <madison.sss.ma@gmail.com>
2023-10-03 14:55:13 +1300

    feat: add 2 week (#50)

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -2,6 +2,7 @@ export const ONE_MIN = 1000 * 60
 export const ONE_HOUR = ONE_MIN * 60
 export const ONE_DAY = ONE_HOUR * 24
 export const SEVEN_DAY = ONE_DAY * 7
+export const TWO_WEEK_DAY = ONE_DAY * 14
 export const ONE_MONTH = ONE_DAY * 31
 
 export const ONE_WEEK = SEVEN_DAY;
