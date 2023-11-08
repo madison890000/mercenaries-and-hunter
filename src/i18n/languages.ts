31b82aba6b5c416ae211f82d14ff0eb4d6a77094
madison <madison.sss.ma@gmail.com>
2023-11-08 23:41:43 +1300

    feat: remove cn and remove translate website

--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -1,17 +1,15 @@
 import enUS from './locales/en-US.json';
-import zhCN from './locales/zh-CN.json';
 import zhTW from './locales/zh-TW.json';
 import deDE from './locales/de-DE.json';
 import frFR from './locales/fr-FR.json';
 import jaJP from './locales/ja-JP.json';
 import koKR from './locales/ko-KR.json';
 
-export type LocalesKey = 'en-US' | 'zh-CN' | 'zh-TW' | 'de-DE' | 'fr-FR' | 'ja-JP' | 'ko-KR';
+export type LocalesKey = 'en-US' | 'zh-TW' | 'de-DE' | 'fr-FR' | 'ja-JP' | 'ko-KR';
 export const locales: {
   [key in LocalesKey]: Record<string, string>;
 } = {
   'en-US': enUS,
-  'zh-CN': zhCN,
   'zh-TW': zhTW,
   'de-DE': deDE,
   'fr-FR': frFR,
@@ -23,7 +21,6 @@ export const APP_LOCALES: {
   [key in LocalesKey]: string;
 } = {
   'en-US': 'English',
-  'zh-CN': '中文简体',
   'zh-TW': '中文繁体',
   'de-DE': 'Deutsch',
   'fr-FR': 'Français',
