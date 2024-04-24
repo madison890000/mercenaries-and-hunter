8ddeb29ac2b5674b7b490e5511ba8a2b5385b7f5
madisonmamott <zusheng.ma@mottmac.com>
2024-04-24 23:11:11 +1200

    remove mock

--- src/services/mh.ts
+++ src/services/mh.ts
@@ -4,28 +4,6 @@ import { IAppliedInExtension } from '../types';
 export const getSendList = async () => {
   if (!chrome?.runtime) {
     return [
-      {
-        "id": "2024-04-24T10:33:39.801Z0.868",
-        "originUrl": "http://localhost:8000/#/",
-        "site": "http://localhost:8000/#/",
-        "time": "2024-04-24T10:33:39.800Z",
-        "title": "M&H"
-      },
-      {
-        "id": "2024-04-24T10:34:39.801Z0.868",
-        "originUrl": "http://localhost:8000/#/",
-        "site": "http://localhost:8000/#/",
-        "time": "2024-04-24T10:33:39.800Z",
-        "title": "M&H"
-      },
-      {
-        "id": "2024-04-24T10:35:39.801Z0.868",
-        "originUrl": "http://localhost:8000/#/",
-        "site": "http://localhost:8000/#/",
-        "time": "2024-04-24T10:33:39.800Z",
-        "title": "M&H"
-      },
-
     ];
   }
   return new Promise<IAppliedInExtension[]>((resolve, reject) => {
@@ -45,5 +23,5 @@ export const getSendList = async () => {
 };
 
 export const hasInstallChromeExtension = () => {
-  return true ||!!chrome?.runtime;
+  return !!chrome?.runtime;
 };
